dd45cbd489f63e390227d1854f1044ca522f4e77
Kexin Qiu <kristenqiu19@gmail.con>
2022-04-14 15:53:55 -0400

    a9

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import helloController from "./controller/hello-controller.js";
 import userController from "./controller/user-controller.js";
 import tuitsController from "./controller/tuits-controller.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+const CONNECTION_STRING ='mongodb+srv://kexinqiu:qkx123456789@cluster0.3evfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     || 'mongodb://localhost:27017/webdev'
 
 mongoose.connect(CONNECTION_STRING);
